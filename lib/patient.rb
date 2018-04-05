d212ce19ad77c3ff356715c4a6867fbcba1fd781
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-04-05 00:08:14 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -5,4 +5,14 @@ class Patient
     @name = name
     @appointments = []
   end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+  end
+
+  def appointments
+    @appointments
+  end
+
 end
